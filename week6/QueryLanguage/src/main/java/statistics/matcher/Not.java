c2ba95c23b5f86b8514571eae20aaf1fe0829652
Sami Nurmivaara <sami.nurmivaara@helsinki.fi>
2018-12-09 19:07:20 +0200

    Week 6 ex. 3 done

---
+++ week6/QueryLanguage/src/main/java/statistics/matcher/Not.java
@@ -0,0 +1,19 @@
+package statistics.matcher;
+
+import statistics.Player;
+
+public class Not implements Matcher {
+    Matcher matcher;
+
+    public Not(Matcher m) {
+        this.matcher = m;
+    }
+
+    @Override
+    public boolean matches(Player p) {
+        if (!matcher.matches(p)) {
+            return true;
+        }
+        return false;
+    }
+}
